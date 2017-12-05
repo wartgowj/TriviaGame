ace9677a96de42047ad83171d7ad20bd9f3ee87b
wartgowj <wartgowj@gmail.com>
2017-12-04 21:49:29 -0800

    added functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,61 @@ $(document).ready(function() {
 var correct
 var incorrect
 var time
+var currentQuestion = 0
+
+var questionsList = [{
+	question: "Who developed the equation to calculate horsepower?",
+	answerList: ["Thomas Edison", "Nikola Tesla", "James Watt", "Albert Einstein"],
+	answer: 2
+},{
+	question: "One horsepower is equal to _______ watts of electricity.",
+	answerList: ["746", "1280", "57", "5325"],
+	answer: 0
+},{
+	question: "A supercharger creates more horsepower by forcing more _______ into an engine.",
+	answerList: ["Gasoline", "Air", "Oil", "Water"],
+	answer: 1
+},{
+	question: "What is used to power a turbocharger?",
+	answerList: ["Drivebelt", "Battery", "Exhaust Gases", "Gasoline"],
+	answer: 2
+},{
+	question: "What is the purpose of a camshaft?",
+	answerList: ["Pump Coolant", "Charge Battery", "Move Pistons", "Open Valves"],
+	answer: 3
+},{
+	question: "Which of these is not needed on a diesel engine?",
+	answerList: ["Sparkplugs", "Alternator", "Water Pump", "Pistons"],
+	answer: 0
+},{
+	question: "On average, how many horespower does a Top fuel dragster have?",
+	answerList: ["500", "10000+", "1000", "5000"],
+	answer: 1
+},{
+	question: "Nitrous oxide increases horsepower by providing more what?",
+	answerList: ["Hydrogen", "Nitrogen", "Oxygen", "Carbon Dioxide"],
+	answer: 2
+},{
+	question: "What does an engine need in order to make more power?",
+	answerList: ["More Air and Less Fuel", "More Air and More Fuel", "Less Air and Less Fuel", "Less Air and More Fuel"],
+	answer: 1
+},{
+	question: "A performance exhaust system increases power by reducing what?",
+	answerList: ["Cylinder Pressure", "Fuel Consumption", "Pollution", "Back Pressure"],
+	answer: 3
+},{
+	question: "High octane fuel is _______ than regular fuel.",
+	answerList: ["More Stable", "More Powerful", "Cleaner", "Higher Quality"],
+	answer: 0
+},{
+	question: "The amount of pressure created by a turbocharger or supercharger is referred to as what?",
+	answerList: ["Nitro", "Boost", "Back Pressure", "Compression"],
+	answer: 1
+},{
+	question: "A performance camshaft adds horsepower by increasing valve lift and _______?",
+	answerList: ["Fuel Pressure", "Compression Ratio", "Fuel Economy", "Duration"],
+	answer: 3
+}];
 
 	function reset(){
 		$('#currentQuestion').hide();
@@ -20,6 +75,25 @@ var time
 		$('#submitButton').show();
 		$('#startButton').hide();
 		intervalId = setInterval(countDown, 1000);
+		generateQuestion();
+	}
+
+	function generateQuestion(){
+		$('#currentQuestion').text(questionsList[currentQuestion].question);
+		$('#choice1').text(questionsList[currentQuestion].answerList[0]);
+		$('#choice2').text(questionsList[currentQuestion].answerList[1]);
+		$('#choice3').text(questionsList[currentQuestion].answerList[2]);
+		$('#choice4').text(questionsList[currentQuestion].answerList[3]);
+	}
+
+	function submit(){
+		clearInterval(intervalId);
+		time = 11;
+		currentQuestion++;
+		generateQuestion();
+		intervalId = setInterval(countDown, 1000);
+		countDown();
+		
 	}
 
 	function countDown(){
@@ -27,11 +101,15 @@ var time
 		$('#timer').html("Time remaining: " + time + " seconds");
 		if (time <= 0){
 			clearInterval(intervalId);
-		}
+		}	
 	}
 
-	$('#startButton').on("click",function(){
+	$('#startButton').on("click", function(){
 		start();
+	})
+
+	$('#submitButton').on("click", function(){
+		submit();
 	})
 
 reset()
